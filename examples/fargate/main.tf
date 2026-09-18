a6667660c359c34461a389ba2a5e7a8cd4820f26
vishwanathacharya09 <vishwanath.acharya@clouddrove.com>
2026-09-19 00:22:43 +0530

    chore: restore fargate example

---
+++ examples/fargate/main.tf
@@ -0,0 +1,183 @@
+##---------------------------------------------------------------------------------------------------------------------------
+## Provider block added, Use the Amazon Web Services (AWS) provider to interact with the many resources supported by AWS.
+##--------------------------------------------------------------------------------------------------------------------------
+provider "aws" {
+  region = "eu-west-1"
+}
+
+locals {
+  vpc_cidr_block        = module.vpc.vpc_cidr_block
+  additional_cidr_block = "172.16.0.0/16"
+  environment           = "test"
+  label_order           = ["name", "environment"]
+}
+##---------------------------------------------------------------------------------------------------------------------------
+## A VPC is a virtual network that closely resembles a traditional network that you'd operate in your own data center.
+##--------------------------------------------------------------------------------------------------------------------------
+module "vpc" {
+  source  = "clouddrove/vpc/aws"
+  version = "2.0.5"
+
+  name        = "vpc"
+  repository  = "https://github.com/clouddrove/terraform-aws-vpc"
+  environment = local.environment
+  label_order = local.label_order
+  cidr_block  = "10.10.0.0/16"
+}
+
+##-----------------------------------------------------
+## A subnet is a range of IP addresses in your VPC.
+##-----------------------------------------------------
+module "subnets" {
+  source  = "clouddrove/subnet/aws"
+  version = "2.0.3"
+
+  name                = "subnets"
+  repository          = "https://github.com/clouddrove/terraform-aws-subnet"
+  environment         = local.environment
+  label_order         = local.label_order
+  nat_gateway_enabled = true
+  availability_zones  = ["eu-west-1a", "eu-west-1b"]
+  vpc_id              = module.vpc.vpc_id
+  cidr_block          = module.vpc.vpc_cidr_block
+  type                = "public-private"
+  igw_id              = module.vpc.igw_id
+  ipv6_cidr_block     = module.vpc.ipv6_cidr_block
+}
+
+# ################################################################################
+# Security Groups module call
+################################################################################
+
+module "sg_lb" {
+  source  = "clouddrove/security-group/aws"
+  version = "2.0.3"
+
+  name        = "ssh"
+  environment = local.environment
+  label_order = local.label_order
+  vpc_id      = module.vpc.vpc_id
+
+  new_sg_ingress_rules = [
+    {
+      key                          = "lb-ssh-vpc"
+      ip_protocol                  = "tcp"
+      from_port                    = 22
+      to_port                      = 22
+      cidr_ipv4                    = local.vpc_cidr_block
+      cidr_ipv6                    = null
+      prefix_list_id               = null
+      referenced_security_group_id = null
+      description                  = "Allow ssh traffic from VPC"
+      tags                         = {}
+    },
+    {
+      key                          = "lb-ssh-additional"
+      ip_protocol                  = "tcp"
+      from_port                    = 22
+      to_port                      = 22
+      cidr_ipv4                    = local.additional_cidr_block
+      cidr_ipv6                    = null
+      prefix_list_id               = null
+      referenced_security_group_id = null
+      description                  = "Allow ssh traffic from additional CIDR"
+      tags                         = {}
+    }
+  ]
+
+  new_sg_egress_rules = [
+    {
+      key                          = "lb-egress-vpc"
+      ip_protocol                  = "tcp"
+      from_port                    = 22
+      to_port                      = 22
+      cidr_ipv4                    = local.vpc_cidr_block
+      cidr_ipv6                    = null
+      prefix_list_id               = null
+      referenced_security_group_id = null
+      description                  = "Allow ssh outbound traffic to VPC"
+      tags                         = {}
+    },
+    {
+      key                          = "lb-egress-additional"
+      ip_protocol                  = "tcp"
+      from_port                    = 22
+      to_port                      = 22
+      cidr_ipv4                    = local.additional_cidr_block
+      cidr_ipv6                    = null
+      prefix_list_id               = null
+      referenced_security_group_id = null
+      description                  = "Allow ssh outbound traffic"
+      tags                         = {}
+    }
+  ]
+}
+
+####----------------------------------------------------------------------------------
+## This terraform module is used for requesting or importing SSL/TLS certificate with validation.
+####----------------------------------------------------------------------------------
+module "acm" {
+  source  = "clouddrove/acm/aws"
+  version = "1.4.2"
+
+  name        = "certificate"
+  environment = local.environment
+  label_order = local.label_order
+
+  enable_aws_certificate    = true
+  domain_name               = "ld.clouddrove.ca"
+  subject_alternative_names = ["*.ld.clouddrove.ca"]
+  validation_method         = "DNS"
+  enable_dns_validation     = true
+}
+
+##-----------------------------------------------------------------------------
+## ecs module call.
+##-----------------------------------------------------------------------------
+module "ecs" {
+  source = "../../"
+
+  ## Tags
+  name        = "ecs-fargate"
+  repository  = "https://github.com/clouddrove/terraform-aws-ecs"
+  environment = local.environment
+  label_order = local.label_order
+  enabled     = true # set to true after VPC, Subnets and Security Groups gets created
+
+  ## Network
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.subnets.private_subnet_id
+
+  ## EC2
+  lb_security_group        = module.sg_lb.security_group_id
+  lb_subnet                = module.subnets.public_subnet_id
+  listener_certificate_arn = module.acm.arn
+
+  ## Fargate Cluster
+  fargate_cluster_enabled = true
+  ecs_settings_enabled    = "enabled"
+  fargate_cluster_cp      = ["FARGATE", "FARGATE_SPOT"]
+
+  ## Service
+  fargate_service_enabled          = true
+  desired_count                    = 4
+  assign_public_ip                 = true
+  propagate_tags                   = "TASK_DEFINITION"
+  scheduling_strategy              = "REPLICA"
+  container_name                   = "nginx"
+  container_port                   = 80
+  target_type                      = "ip"
+  weight_simple                    = 1
+  weight_spot                      = 2
+  base                             = 1
+  fargate_capacity_provider_simple = "FARGATE"
+  fargate_capacity_provider_spot   = "FARGATE_SPOT"
+
+  ## Task Definition
+  fargate_td_enabled       = true
+  cpu                      = 512
+  network_mode             = "bridge"
+  memory                   = 1024
+  file_name                = "./td-fargate.json"
+  container_log_group_name = "fargate-container-logs"
+}
