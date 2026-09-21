25e718d09096cc678a243a776443bc7d77355c1e
vishwanathacharya09 <vishwanath.acharya@clouddrove.com>
2026-09-21 18:25:53 +0530

    fix: add AWSServiceRoleForAutoScaling permissions to KMS key policy for EBS encryption

--- examples/ecs-cluster/main.tf
+++ examples/ecs-cluster/main.tf
@@ -197,6 +197,37 @@ data "aws_iam_policy_document" "kms" {
     actions   = ["kms:*"]
     resources = ["*"]
   }
+  statement {
+    sid    = "Allow AutoScaling to use the key"
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling"]
+    }
+    actions = [
+      "kms:Encrypt",
+      "kms:Decrypt",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*",
+      "kms:DescribeKey"
+    ]
+    resources = ["*"]
+  }
+  statement {
+    sid    = "Allow AutoScaling to create grants"
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling"]
+    }
+    actions   = ["kms:CreateGrant"]
+    resources = ["*"]
+    condition {
+      test     = "Bool"
+      variable = "kms:GrantIsForAWSResource"
+      values   = ["true"]
+    }
+  }
 }
 
 ##---------------------------------------------------------------------------------------------------------------------------
